02db1824d484497fe7d56157570f6b28fd2803c4
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-08-06 20:35:41 +0200

    Added traffic

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-    constructor(x, y, width, height) {
+    constructor(x, y, width, height, type, maxSpeed) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -7,24 +7,29 @@ class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
         this.angle = 0;
 
         this.damaged = false;
 
-        this.senser = new Sensors(this);
+        if (type !== "DUMMY") {
+            this.sensor = new Sensors(this);
+        }
 
-        this.controls = new Controls();
+        this.controls = new Controls(type);
     }
 
-    update(roadBorders) {
+    update(roadBorders, traffic) {
         if (!this.damaged) {
             this.#move();
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders);
+            this.damaged = this.#assessDamage(roadBorders, traffic);
         }
-        this.senser.update(roadBorders);
+
+        if (this.sensor) {
+            this.sensor.update(roadBorders, traffic);
+        }
     }
 
     #createPolygon() {
@@ -95,11 +100,11 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx){
+    draw(ctx, color){
         if (this.damaged) {
             ctx.fillStyle = "gray";
         } else {
-            ctx.fillStyle = "black";
+            ctx.fillStyle = color;
         }
 
         ctx.beginPath();
@@ -110,12 +115,20 @@ class Car {
         }
         ctx.fill();
 
-        this.senser.draw(ctx);
+        if (this.sensor) {
+            this.sensor.draw(ctx);
+        }
     }
 
-    #assessDamage(roadBorders) {
+    #assessDamage(roadBorders, traffic) {
         for (let i = 0; i < roadBorders.length; i++) {
             if (polysIntersect(this.polygon, roadBorders[i])) {
+                return true;
+            }
+        }
+
+        for (let i = 0; i < traffic.length; i++) {
+            if (polysIntersect(this.polygon, traffic[i].polygon)) {
                 return true;
             }
         }
